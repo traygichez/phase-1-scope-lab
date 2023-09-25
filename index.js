489b7e602d03c08048eabcf7853cc511d6637851
tray <onesimus.githua@student.moringaschool.com>
2023-09-25 11:39:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+                   bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer='everlyn';
+
+function changeLeastFavoriteCustomer(){
+              leastFavoriteCustomer='kamau';
+}
